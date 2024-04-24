67c31fd24dcf5f4ec6e54faff6b5c274e0991cc7
VinitaSonwane <VinitaSonwane>
2024-04-24 14:03:33 +0530

    commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'; // Import React and useState
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className='bg-green-400 text'>Tailwind test</h1>
-    </>
-  )
+    <div>
+      <h1 className='bg-green-200'>Tailwind test</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
